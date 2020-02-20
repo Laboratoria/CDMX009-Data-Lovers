055447b57eeac310a0eeb311f61e10a3cedbd407
Adriana Hernandez <adrianahernandez@Adrianas-MacBook-Air.local>
2020-02-20 08:35:10 -0600

    Modified data for Mexico

--- src/data.js
+++ src/data.js
@@ -1,4 +1,10 @@
-/* Manejo de data */
+// import data from './data/injuries/injuries.js';
+// import data from './data/lol/lol.js';
+// import data from './data/patient/patient.js';
+// import data from './data/pokemon/pokemon.js';
+// import data from './data/rickandmorty/rickandmorty.js';
+// import data from './data/steam/steam.js';
+// import data from './data/steam/worldbank.js';
 
 // esta es una función de ejemplo
 
